65f203f80855fb5a855c8a37a8fbae3efb3d10dc
UENO, M. <eunosium-1128@live.jp>
2026-07-11 17:19:38 +0000

    Add python-deps.sh script for managing Python dependencies

---
+++ python-deps.sh
@@ -0,0 +1,9 @@
+if ! python3 -c "import requirements_parser" >/dev/null 2>&1; then
+  pip3 install requirements-parser
+fi
+
+flatpak-builder-tools/pip/flatpak-pip-generator \
+  --requirements-file requirements-flatpak.txt \
+  --output python-deps \
+  --yaml \
+  --runtime org.gnome.Sdk//50
