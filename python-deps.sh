d53f0c9d67ff4727aae59c4cd41b7082ba856f01
UENO, M. <eunosium-1128@live.jp>
2026-07-11 17:45:05 +0000

    Update python-deps.sh and python-deps.yaml to enhance dependency management and specify preferred wheels

--- python-deps.sh
+++ python-deps.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 if ! python3 -c "import requirements_parser" >/dev/null 2>&1; then
-  pip3 install requirements-parser
+    pip3 install requirements-parser
 fi
 
 flatpak-builder-tools/pip/flatpak-pip-generator \
-  --requirements-file requirements-flatpak.txt \
-  --output python-deps \
-  --yaml \
-  --runtime org.gnome.Sdk//50
+    --requirements-file requirements-flatpak.txt \
+    --prefer-wheels=cryptography,lxml \
+    --runtime org.gnome.Sdk//50 \
+    --output python-deps \
+    --yaml
