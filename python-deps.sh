c1eeb6e58d9a0bde90a3f2be2a328a0ceb91a40f
UENO, M. <eunosium-1128@live.jp>
2026-07-11 18:11:16 +0000

    Update python-deps.sh to include lxml in the ignore-installed option

--- python-deps.sh
+++ python-deps.sh
@@ -7,6 +7,7 @@ fi
 flatpak-builder-tools/pip/flatpak-pip-generator \
     --requirements-file requirements.txt \
     --prefer-wheels=cryptography \
+    --ignore-installed=lxml \
     --runtime org.gnome.Sdk//50 \
     --output python-deps \
     --yaml
