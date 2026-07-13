896d793b5926728f47e69eaa356bcf830f43026e
UENO, M. <eunosium-1128@live.jp>
2026-07-13 11:16:15 +0900

    Update python-deps.sh to avoid building cryptography

--- python-deps.sh
+++ python-deps.sh
@@ -6,6 +6,7 @@ fi
 
 # lxml is present in org.gnome.Sdk//50 as a build-time dependency,
 # but not in org.gnome.Platform//50, so it must be installed into /app.
+# Avoid building cryptography from source, which requires maturin and Rust.
 flatpak-builder-tools/pip/flatpak-pip-generator \
     --requirements-file requirements.txt \
     --prefer-wheels cryptography \
