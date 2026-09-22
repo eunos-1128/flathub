2b8eb776a3d588b2f36204d305da748f599e7884
UENO, M. <eunosium-1128@live.jp>
2026-09-22 18:40:12 +0900

    Update runtime version in python-deps.sh

--- python-deps.sh
+++ python-deps.sh
@@ -4,14 +4,13 @@ if ! python3 -c "import requirements_parser" >/dev/null 2>&1; then
     python3 -m pip install requirements-parser
 fi
 
-# pygobject and pycairo are already in org.gnome.Platform//50, so skip them.
+# pygobject and pycairo are already in org.gnome.Platform//51, so skip them.
 # lxml is in the Sdk but not the Platform runtime, so it still needs installing.
 # Avoid building cryptography from source, which requires maturin and Rust.
 flatpak-builder-tools/pip/flatpak-pip-generator \
     --requirements-file requirements.txt \
     --ignore-pkg 'pygobject,pycairo' \
-    --prefer-wheels cryptography \
     --ignore-installed lxml \
-    --runtime org.gnome.Sdk//50 \
+    --runtime org.gnome.Sdk//51 \
     --output python-deps \
     --yaml
